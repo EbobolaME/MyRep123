432102c09dffabb36975ba1512136672bb9e1668
20405 <20405@1k129s8.dune.edu>
2023-03-07 14:37:56 +0200

    added белебирда

--- src/javaapplicationlaba1/JavaApplicationlaba1.java
+++ src/javaapplicationlaba1/JavaApplicationlaba1.java
@@ -16,6 +16,9 @@ public class JavaApplicationlaba1 {
      */
     public static void main(String[] args) {
         System.out.println("Hello world");
+         System.out.println("hadigfighff");
+        
+        
        
             
                 
