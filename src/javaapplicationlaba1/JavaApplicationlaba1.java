d71c728af277138895bea9c4e47166f5a0f034be
20405 <20405@1k129s8.dune.edu>
2023-03-07 14:37:06 +0200

    added hello world

---
+++ src/javaapplicationlaba1/JavaApplicationlaba1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplicationlaba1;
+
+/**
+ *
+ * @author 20405
+ */
+public class JavaApplicationlaba1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+       
+            
+                
+    }
+    
+}
